555c54936ce9da9c621b58e58d703dad9ea67f4e
jesgogu27 <jesgogu@hotmail.com>
2020-06-25 01:56:09 -0500

    Bubble

---
+++ 0-bubble_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+* bubble_sort - ascending order
+*@array: elements to order
+*@size: size of array.
+*
+*/
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int aux;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0, i < size; i++)
+	{
+		for (j = 0; i < (size - i - 1); j++)
+		{
+
+			if (array[j] > array[j + 1])
+			{
+				aux = array[j];
+				array = array[j + 1];
+				array[j + 1] = aux;
+				print_array(array, size);
+
+			}
+		}
+	}
+}
+
---
+++ README.md
@@ -0,0 +1,8 @@
+﻿**README**
+
+For this project we wast to learned
+    • At least four different sorting algorithms
+    • What is the Big O notation, and how to evaluate the time complexity of an algorithm
+    • How to select the best sorting algorithm for a given input
+    • What is a stable sorting algorithm
+
