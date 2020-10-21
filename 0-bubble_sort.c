b892d8c545e4c2356d1626d6a1d465765b64e9ac
jesgogu27 <jesgogu@hotmail.com>
2020-10-21 04:46:15 -0500

    betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,26 +9,23 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int aux;
+size_t i, j;
+int aux;
 
-	if (array == NULL || size < 2)
-		return;
+if (array == NULL || size < 2)
+return;
 
-	for (i = 0; i < size; i++)
-	{
-		for (j = 0; j < (size - i - 1); j++)
-		{
-
-			if (array[j] > array[j + 1])
-			{
-				aux = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = aux;
-				print_array(array, size);
-
-			}
-		}
-	}
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < (size - i - 1); j++)
+{
+if (array[j] > array[j + 1])
+{
+aux = array[j];
+array[j] = array[j + 1];
+array[j + 1] = aux;
+print_array(array, size);
 }
-
+}
+}
+}
